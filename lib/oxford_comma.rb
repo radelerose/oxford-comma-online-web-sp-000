19fed72ad49d347a048522b0612fccfae8ad3ad7
Radele Rose <radele.rose@gmail.com>
2021-02-08 05:58:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
-array = "kiwi"
 
 def oxford_comma(array)
-"{array}"
+if array.length == 2
+  return "#{array[0]} and #{array[1]}"
+elsif 2 < array.length
+  array[-1].insert(0, "and ")
+end
+array.join(", ")    
 end
